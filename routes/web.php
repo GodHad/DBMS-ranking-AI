28b83cd26bb4109fbb4d54587cb1c621da8a56b3
GodHad <sunharius@gmail.com>
2024-10-31 11:17:52 -0700

    change user api

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,11 @@ use Illuminate\Support\Facades\Auth;
 Route::get('/api/user', function() {
     $user = Auth::guard('web')->user();
     $admins = env('admin');
-    if ($user) $user->admin = strpos($admins, $user->email) !== false;
-    $userRole = UserRole::where('user_id', $user->id)->first();
-    $user->author = $userRole->author;
+    if ($user) {
+        $user->admin = strpos($admins, $user->email) !== false;
+        $userRole = UserRole::where('user_id', $user->id)->first();
+        $user->author = $userRole->author;
+    }
     return response()->json(['user' => $user]);
 });
 
